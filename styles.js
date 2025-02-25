82607ac96597ef9737c90a1948a7681d6271803b
Leo_Alexandros <66133763+LeoAcerakGames@users.noreply.github.com>
2025-02-25 15:21:15 +0100

    Add files via upload

--- styles.js
+++ styles.js
@@ -1,17 +1,1 @@
-function register() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmPassword").value;
-    
-    if (password !== confirmPassword) {
-        alert("Passwords do not match!");
-        return;
-    }
-    
-    // Simulating user registration (in a real system, you would send this data to a server)
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
-    
-    alert("Registration successful!");
-    window.location.href = "home.html";
-}
+
